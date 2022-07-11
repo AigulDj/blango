be84b95984fdc58bf99cfe918b0387ff06aa102c
Aigul Djon <aigul.djon@gmail.com>
2022-07-11 13:28:21 +0000

    Finish custom filters

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+  title = models.CharField(max_length=30)
+  author = models.CharField(max_length=30)
+  published_at = models.DateField()
